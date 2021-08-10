35ff3c5bb2b766b422f655e17653508999435a16
Igor Fragoso <igorfragoso3@gmail.com>
2021-08-10 15:02:20 -0300

    logo and text small fixes

--- src/components/pages/HomePage/Data.js
+++ src/components/pages/HomePage/Data.js
@@ -26,31 +26,3 @@ export const homeObjTwo = {
   img: 'images/midiaimpressa.jpg',
   alt: 'Placa Midia Impressa',
 };
-
-export const homeObjThree = {
-  lightBg: true,
-  lightText: false,
-  lightTextDesc: false,
-  topLine: 'EASY SETUP',
-  headline: 'Super fast and simple onboarding process',
-  description:
-    "Get everything set up and ready in under 10 minutes. All you need to do is add your information and you're ready to go.",
-  buttonLabel: 'Start Now',
-  imgStart: 'start',
-  img: 'images/svg-7.svg',
-  alt: 'Vault',
-};
-
-export const homeObjFour = {
-  lightBg: false,
-  lightText: true,
-  lightTextDesc: true,
-  topLine: 'DATA ANALYTICS',
-  headline: 'Every transaction is stored on our secure cloud database',
-  description:
-    'Never ever have to worry again about saved reciepts. We store your data, so you can access it anytime.',
-  buttonLabel: 'Sign Up Now',
-  imgStart: 'start',
-  img: 'images/svg-8.svg',
-  alt: 'Vault',
-};
